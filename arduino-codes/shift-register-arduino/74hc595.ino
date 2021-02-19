7e5c4f40f214a85799406debec90c3afdb8ffad2
Vinicius-Moraes20 <78623970+Vinicius-Moraes20@users.noreply.github.com>
2021-02-19 02:15:31 -0300

    Arquivos atualizados e registradores de deslocamento

--- arduino-codes/shift-register-arduino/74hc595.ino
+++ arduino-codes/shift-register-arduino/74hc595.ino
@@ -1,17 +1,17 @@
 #define clk     2  //Definindo o pino clock do 74HC585    
-#define letch   3  //Definindo o pino letch do 74HC585
-#define data    4  //Definindo o pino data  do 74HC585
+#define data    3  //Definindo o pino data  do 74HC585
+#define letch   4  //Definindo o pino letch do 74HC585
 
 void setup() {
-	pinMode(clk,   OUTPUT);  // |
+  pinMode(clk,   OUTPUT);  // |
   pinMode(letch, OUTPUT);  // | Definindo pinos de saida
   pinMode(data,  OUTPUT);  // |
 }
 
 void loop() {
-    shiftOut(data, clk, LSBFIRST, 0x66);
+    shiftOut(data, clk, LSBFIRST, 0x3C);
     attShift();
-    shiftOut(data, clk, LSBFIRST, 0xD0);
+    shiftOut(data, clk, LSBFIRST, 0xC3);
     attShift();
 }
 
@@ -19,5 +19,6 @@ void attShift() {
   digitalWrite(letch, LOW);   // |
   digitalWrite(letch, HIGH);  // | Atualizando dados
   digitalWrite(letch, LOW);   // |
+  delay(1000);
   return 0;
 }
