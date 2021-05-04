908095c48d805ab90232267d83714d4e617e793a
Vinicius-Moraes20 <78623970+Vinicius-Moraes20@users.noreply.github.com>
2021-05-03 21:25:46 -0300

    aulas curso de JS

---
+++ programming/java-script/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} KM/h`)
+
+if (val >= 60) {
+    console.log("Você ultrapassou a velocidade permitida! MULTADO!")
+}
+
+console.log('Dirija sempre usando cinto de segurança')
