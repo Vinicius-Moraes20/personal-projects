9f4cfbecc9e4beb687abaa1239f8232147b40f11
Vinicius-Moraes20 <78623970+Vinicius-Moraes20@users.noreply.github.com>
2021-05-31 20:57:52 -0300

    Python aula 6

---
+++ programming/python/aula06/ex01.py
@@ -0,0 +1,6 @@
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite outro numero: "))
+
+s = n1 + n2
+
+print("A soma entre {} e {} é {}".format(n1, n2, s))
---
+++ programming/python/aula06/ex02.py
@@ -0,0 +1,4 @@
+n = input("Digite algo: ")
+print(n.isnumeric()) #valor true caso for numero
+print(n.isalpha())   #valor true caso for letra
+print(n.isalnum())   #valro true caso for alfanumérico
