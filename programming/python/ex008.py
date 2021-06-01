d47d8696c3e29a1e8e6e3e8625d6610d89dd820f
Vinicius-Moraes20 <78623970+Vinicius-Moraes20@users.noreply.github.com>
2021-06-01 16:43:00 -0300

    Python ex008

---
+++ programming/python/ex008.py
@@ -0,0 +1,8 @@
+m = float(input("Digite a metragem a ser convertida: "))
+cm = m * 100
+mm = m * 1000
+
+
+print("Metros: {}m".format(m))
+print("Centimetros: {}cm".format(cm))
+print("Milimetros: {}mm".format(mm))
