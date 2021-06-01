52b05cdff15b229cb7ba2270835d341c03187b68
Vinicius-Moraes20 <78623970+Vinicius-Moraes20@users.noreply.github.com>
2021-06-01 17:06:20 -0300

    Python ex011

---
+++ programming/python/ex011.py
@@ -0,0 +1,4 @@
+largura = float(input("Digite a largura da parede: "))
+altura  = float(input("Digite a altura da parede: "))
+valTinta = (largura * altura) / 2
+print("Voce precida de {}L de tinta para pintar a parede".format(valTinta))
