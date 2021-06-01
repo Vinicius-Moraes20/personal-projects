88f16d42a5066f73c9b23f9ed0f2bd061bafaff0
Vinicius-Moraes20 <78623970+Vinicius-Moraes20@users.noreply.github.com>
2021-05-31 21:04:13 -0300

    Python ex004

---
+++ programming/python/ex004.py
@@ -0,0 +1,7 @@
+valor = input("Digite algo: ")
+print("Valor numérico:", valor.isnumeric())
+print("Valor Alfa:", valor.isalpha())
+print("Valor Alfanumérico:", valor.isalnum())
+print("Valor ASCII:", valor.isascii())
+print("Valor Decimal", valor.isdecimal())
+print("Valor Printavel", valor.isprintable())
