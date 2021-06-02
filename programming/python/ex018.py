abd9e56958d19a83141629255cec8ae7af7343cc
Vinicius-Moraes20 <78623970+Vinicius-Moraes20@users.noreply.github.com>
2021-06-02 17:57:53 -0300

    Python ex018

---
+++ programming/python/ex018.py
@@ -0,0 +1,5 @@
+import math
+an = float(input("Digite um angulo qualquer: "))
+print("Seno: {:.2f}".format(math.sin(math.radians(an))))
+print("Cosseno: {:.2f}".format(math.cos(math.radians(an))))
+print("Tangente: {:.2f}".format(math.tan(math.radians(an))))
