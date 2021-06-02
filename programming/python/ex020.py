6754a1468812934a08adc346ca4c1c5e1270bb38
Vinicius-Moraes20 <78623970+Vinicius-Moraes20@users.noreply.github.com>
2021-06-02 18:20:06 -0300

    Python ex020

---
+++ programming/python/ex020.py
@@ -0,0 +1,12 @@
+import random
+
+aluno1 = input("Digite o nome do primeiro aluno: ")
+aluno2 = input("Digite o nome do segundo aluno: ")
+aluno3 = input("Digite o nome do terceiro aluno: ")
+aluno4 = input("Digite o nome do quarto aluno: ")
+
+lista = [aluno1, aluno2, aluno3, aluno4]
+random.shuffle(lista)
+
+print("A ordem de aprensentação é: ", end=" ")
+print(lista)
