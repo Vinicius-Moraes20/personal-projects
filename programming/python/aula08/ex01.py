4214e5aea2eed67a921cec4b062aab59d2dd882c
Vinicius-Moraes20 <78623970+Vinicius-Moraes20@users.noreply.github.com>
2021-06-01 21:06:21 -0300

    Python aula8

---
+++ programming/python/aula08/ex01.py
@@ -0,0 +1,4 @@
+from math import sqrt, floor
+n = int(input("Digite um numero: "))
+raiz = sqrt(n)
+print("A raiz de {} é igual a {}".format(n, floor(raiz)))
---
+++ programming/python/aula08/ex03.py
@@ -0,0 +1,3 @@
+import emoji
+print("TESTE!!!!!!!!!!!!!!!!!!!!!")
+print(emoji.emojize("Olá Mundo! :simple_smile:", use_aliases=True))
