9d79b201b1aafc12115e591839707dcb97f044ba
Vinicius-Moraes20 <78623970+Vinicius-Moraes20@users.noreply.github.com>
2021-06-02 18:10:15 -0300

    Python ex019

---
+++ programming/python/ex019.py
@@ -0,0 +1,11 @@
+import random
+
+aluno1 = input("Digite o nome do primeiro aluno: ")
+aluno2 = input("Digite o nome do segundo aluno: ")
+aluno3 = input("Digite o nome do terceiro aluno: ")
+aluno4 = input("Digite o nome do quarto aluno: ")
+
+lista = [aluno1, aluno2, aluno3, aluno4]
+sorteado = random.choice(lista)
+
+print("O aluno() escolhido(a) foi: {}".format(sorteado))
