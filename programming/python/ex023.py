e64c92aa16a2078291d4af3c48e5c7af878dcebc
Vinicius-Moraes20 <78623970+Vinicius-Moraes20@users.noreply.github.com>
2021-06-07 18:02:09 -0300

    Python ex023

---
+++ programming/python/ex023.py
@@ -0,0 +1,5 @@
+num = int(input("Digite um numero de 4 digitos: "))
+print("Unidade: {}".format(num // 1 % 10))
+print("Dezena: {}".format(num // 10 % 10))
+print("Centena: {}".format(num // 100 % 10))
+print("Milhar: {}".format(num // 1000 % 10))
