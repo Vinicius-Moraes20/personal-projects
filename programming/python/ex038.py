1147f4b45831e4daedd49e6c977f897ce5b61e16
Vinicius-Moraes20 <78623970+Vinicius-Moraes20@users.noreply.github.com>
2021-06-16 17:12:51 -0300

    Python ex038

---
+++ programming/python/ex038.py
@@ -0,0 +1,8 @@
+n1 = int(input("Digite o primeiro numero: "))
+n2 = int(input("Digite o segundo numero: "))
+if n1 > n2:
+    print("O primeiro numero é maior que o segundo")
+elif n1 < n2:
+    print("O segundo numero é maior que o primeiro")
+elif n1 == n2:
+    print("Os dois numeros são idênticos")
